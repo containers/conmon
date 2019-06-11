389581605ce39528b3fc8911ea79ba252a56e99a
Peter Hunt <pehunt@redhat.com>
2019-06-11 16:21:37 -0400

    Add cmd/config/config.go
    Now, we can configure conmon from within the conmon repository.
    This will allow tools to vendor conmon and import the values they need, rather than having the configuration parameters in different repositories
    Also added runner/ package to store the configuration parameters
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

---
+++ cmd/conmon-config/conmon-config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/containers/conmon/runner/config"
+)
+
+func main() {
+	output := `
+#if !defined(CONFIG_H)
+#define CONFIG_H
+
+#define BUF_SIZE %d
+#define STDIO_BUF_SIZE %d
+#define DEFAULT_SOCKET_PATH "%s"
+
+#endif // CONFIG_H
+`
+	if err := ioutil.WriteFile("config.h", []byte(fmt.Sprintf(output, config.BufSize, config.BufSize, config.ContainerAttachSocketDir)), 0644); err != nil {
+		fmt.Errorf(err.Error())
+	}
+}
