d7c90bdc5d17f8f55b17eef89f11e1fc31843425
Peter Hunt <pehunt@redhat.com>
2019-09-17 15:04:35 -0400

    Add CONN_SOCK_BUF_SIZE to config
    instead of having a note that says to sync the value
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- cmd/conmon-config/conmon-config.go
+++ cmd/conmon-config/conmon-config.go
@@ -14,11 +14,12 @@ func main() {
 
 #define BUF_SIZE %d
 #define STDIO_BUF_SIZE %d
+#define CONN_SOCK_BUF_SIZE %d
 #define DEFAULT_SOCKET_PATH "%s"
 
 #endif // CONFIG_H
 `
-	if err := ioutil.WriteFile("config.h", []byte(fmt.Sprintf(output, config.BufSize, config.BufSize, config.ContainerAttachSocketDir)), 0644); err != nil {
+	if err := ioutil.WriteFile("config.h", []byte(fmt.Sprintf(output, config.BufSize, config.BufSize, config.ConnSockBufSize, config.ContainerAttachSocketDir)), 0644); err != nil {
 		fmt.Errorf(err.Error())
 	}
 }
