cbeabba67a3ac5be6d02e19f80a80f27d28ca4e2
Sascha Grunert <sgrunert@suse.com>
2019-10-15 13:08:12 +0200

    Add 'trace' log level support
    The trace log level is currently not supported by conmon, but seems to be
    a valid level for container runtimes like CRI-O and podman. This results
    that the container currently won't start if the log level is set to
    'trace'. Beside this, we now case insensitively compare the log level
    string to make it more fail safe.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,6 @@
 #include "utils.h"
 #include <string.h>
+#include <strings.h>
 
 log_level_t log_level = WARN_LEVEL;
 char *log_cid = NULL;
@@ -14,18 +15,22 @@ void set_conmon_logs(char *level_name, char *cid_, gboolean syslog_)
 	// log_level is initialized as Warning, no need to set anything
 	if (level_name == NULL)
 		return;
-	if (!strcmp(level_name, "error") || !strcmp(level_name, "fatal") || !strcmp(level_name, "panic")) {
+	if (!strcasecmp(level_name, "error") || !strcasecmp(level_name, "fatal") || !strcasecmp(level_name, "panic")) {
 		log_level = EXIT_LEVEL;
 		return;
-	} else if (!strcmp(level_name, "warn") || !strcmp(level_name, "warning")) {
+	} else if (!strcasecmp(level_name, "warn") || !strcasecmp(level_name, "warning")) {
 		log_level = WARN_LEVEL;
 		return;
-	} else if (!strcmp(level_name, "info")) {
+	} else if (!strcasecmp(level_name, "info")) {
 		log_level = INFO_LEVEL;
 		return;
-	} else if (!strcmp(level_name, "debug")) {
+	} else if (!strcasecmp(level_name, "debug")) {
 		log_level = DEBUG_LEVEL;
 		return;
+	} else if (!strcasecmp(level_name, "trace")) {
+		log_level = TRACE_LEVEL;
+		return;
 	}
+	ntracef("set log level to %s", level_name);
 	nexitf("No such log level %s", level_name);
 }
