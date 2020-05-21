58067e44bba2f6fab98fa902a6f13bab9ed21e15
Peter Hunt <pehunt@redhat.com>
2020-05-21 16:39:49 -0400

    only use updated conmon
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- contrib/cirrus/cri-o_test.sh
+++ contrib/cirrus/cri-o_test.sh
@@ -4,20 +4,12 @@ set -e
 source $(dirname $0)/lib.sh
 
 req_env_var "
-SRC $SRC
 CRIO_SRC $CRIO_SRC
-OS_RELEASE_ID $OS_RELEASE_ID
-OS_RELEASE_VER $OS_RELEASE_VER
 "
 
-# Assume cri-o and all dependencies are installed from packages
-# and conmon installed using build_and_replace_conmon()
-export CRIO_BINARY_PATH=/usr/bin/crio
+# Assume conmon is installed using build_and_replace_conmon()
 export CONMON_BINARY=/usr/libexec/crio/conmon
-export PAUSE_BINARY=/usr/libexec/crio/pause
-export CRIO_CNI_PLUGIN=/usr/libexec/cni
 
 echo "Executing cri-o integration tests (typical 10 - 20 min)"
 cd "$CRIO_SRC"
-$CRIO_BINARY_PATH version
-sudo timeout --foreground --kill-after=5m 60m ./test/test_runner.sh
+timeout --foreground --kill-after=5m 60m sudo ./test/test_runner.sh
