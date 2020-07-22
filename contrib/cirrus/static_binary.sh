13e4d496ead78fabf7f83b417bedd97b3b3f4a9a
Chris Evich <cevich@redhat.com>
2020-07-22 10:47:44 -0400

    Cirrus: Nix build follows POLA
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/cirrus/static_binary.sh
+++ contrib/cirrus/static_binary.sh
@@ -3,6 +3,13 @@
 set -e
 source $(dirname $0)/lib.sh
 
-cd $CIRRUS_WORKING_DIR
-$SCRIPT_BASE/config.sh
-make containerized
+podman_run="podman run -i --rm --security-opt label=disable --privileged"
+mkdir -p /var/tmp/nix
+
+set -x
+podman_run -v /var/tmp/nix:/mnt nixos/nix cp -rfT /nix /mnt
+podman_run -v /var/tmp/nix:/nix \
+    -v $CIRRUS_WORKING_DIR:$CIRRUS_WORKING_DIR \
+    -w $CIRRUS_WORKING_DIR \
+    nixos/nix \
+    nix --print-build-logs --option cores 8 --option max-jobs 8 build --file nix/
