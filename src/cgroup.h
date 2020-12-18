e7e84a451798acfa4270d89af5f6c153c6ca6b5b
Giuseppe Scrivano <gscrivan@redhat.com>
2020-12-18 16:20:35 +0100

    cgroup: store used file descriptors
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- src/cgroup.h
+++ src/cgroup.h
@@ -3,6 +3,9 @@
 
 #include <glib.h> /* gboolean */
 
+extern int oom_cgroup_fd;
+extern int oom_event_fd;
+
 void setup_oom_handling(int pid);
 gboolean conn_sock_cb(int fd, GIOCondition condition, gpointer user_data);
 gboolean check_cgroup2_oom();
