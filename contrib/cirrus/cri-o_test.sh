e8793ab39ddfd358e365560aa08a0b3c4d09c5ca
Chris Evich <cevich@redhat.com>
2021-04-09 12:10:36 -0400

    Cirrus: Update VM Images
    Update to F34, and Ubuntu 2104. Also modernize scripts to use
    shared automation library functions.  Lastly, update
    `hack/get_ci_vm.sh` to match functionality of other
    containers-org. repositories.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/cirrus/cri-o_test.sh
+++ contrib/cirrus/cri-o_test.sh
@@ -3,23 +3,25 @@
 set -e
 source $(dirname $0)/lib.sh
 
-req_env_var "
-SRC $SRC
-CRIO_SRC $CRIO_SRC
-OS_RELEASE_ID $OS_RELEASE_ID
-OS_RELEASE_VER $OS_RELEASE_VER
-"
+req_env_var SRC SCRIPT_BASE CRIO_SRC OS_RELEASE_ID OS_RELEASE_VER
 
 cd "$CRIO_SRC"
-case "$OS_REL_VER" in
-    fedora-32)
-        PATCH="$SRC/$SCRIPT_BASE/network_bats.patch"
-        cd "$CRIO_SRC"
-        echo "WARNING: Applying $PATCH"
-        git apply --index --apply --ignore-space-change --recount "$PATCH"
-        ;;
-    *) bad_os_id_ver ;;
-esac
+
+if [[ ! "$OS_RELEASE_ID" =~ fedora ]]; then
+    bad_os_id_ver
+fi
+
+msg "Building binaries required for testing"
+ooe.sh make test-binaries
+
+# TODO: Around the CRI-O 1.16 timeframe, the network tests
+# would fail without this patch.  The same problem did not
+# occur on RHEL/CentOS.  Countless hours were spent debugging
+# but the root-cause was never found.  This is a workaround.
+PATCH="$SRC/$SCRIPT_BASE/network_bats.patch"
+cd "$CRIO_SRC"
+warn "WARNING: Applying $PATCH"
+git apply --index --apply --ignore-space-change --recount "$PATCH"
 
 # Assume cri-o and all dependencies are installed from packages
 # and conmon installed using build_and_replace_conmon()
@@ -28,6 +30,6 @@ export CONMON_BINARY=/usr/libexec/crio/conmon
 export PAUSE_BINARY=/usr/libexec/crio/pause
 export CRIO_CNI_PLUGIN=/usr/libexec/cni
 
-echo "Executing cri-o integration tests (typical 10 - 20 min)"
+msg "Executing cri-o integration tests (typical 10 - 20 min)"
 cd "$CRIO_SRC"
 timeout --foreground --kill-after=5m 60m ./test/test_runner.sh
