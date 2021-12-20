containers/conmon

commit 4d3dba9c4beb7aa7b64a29a5345d2251fc5b92ac

exit: fix race zombie reaper
fix a race condition where all the processes spawned by the OCI
runtime are not terminated yet and reap_children() would not clean
them up.

If an --exit-delay is specified, these proceses would be left
uncleaned until the delay expires.

When used for an exec session, the zombie process would prevent the
container namespaces to be cleaned up.

Closes: https://github.com/containers/conmon/issues/290

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/ctr_exit.c
@@ -133,12 +133,6 @@ void container_exit_cb(G_GNUC_UNUSED GPid pid, int status, G_GNUC_UNUSED gpointe
 
 void do_exit_command()
 {
-	/* even though we've already registered reap_children,
-	 * atexit() runs functions in reverse, so we need to
-	 * manually call here. Repeated calls will not cause trouble
-	 */
-	reap_children();
-
 	if (sync_pipe_fd > 0) {
 		close(sync_pipe_fd);
 		sync_pipe_fd = -1;
@@ -156,9 +150,18 @@ void do_exit_command()
 	if (exit_pid) {
 		int ret, exit_status = 0;
 
-		do
-			ret = waitpid(exit_pid, &exit_status, 0);
-		while (ret < 0 && errno == EINTR);
+		/*
+		 * Make sure to cleanup any zombie process that the container runtime
+		 * could have left around.
+		 */
+		do {
+			int tmp;
+
+			exit_status = 0;
+			ret = waitpid(-1, &tmp, 0);
+			if (ret == exit_pid)
+				exit_status = tmp;
+		} while ((ret < 0 && errno == EINTR) || ret > 0);
 
 		if (exit_status)
 			_exit(exit_status);

commit 74ce165f403fa7632aeb461c5051029f8424e3ec

exit_command: unset subreaper attribute before running exit command
staying as subreaper means the exit command would not be able to start new
daemonizes processes e.g. another podman container if it's been set to restart.

Fixes: 4d3dba9c4beb ("exit: fix race zombie reaper")
Fixes: #310

src/ctr_exit.c
@@ -12,6 +12,7 @@
 #include <glib-unix.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/prctl.h>
 #include <unistd.h>
 
 volatile pid_t container_pid = -1;
@@ -158,6 +159,16 @@ void do_exit_command()
 	close_fd(&attach_socket_fd);
 	close_fd(&console_socket_fd);
 	close_fd(&attach_pipe_fd);
+
+	/*
+	 * We don't want the exit command to be reaped by the parent conmon
+	 * as that would prevent double-fork from doing its job.
+	 * Unfortunately, that also means that any new subchildren from
+	 * still running processes could also get lost
+	 */
+	if (prctl(PR_SET_CHILD_SUBREAPER, 0) != 0) {
+		nwarn("Failed to disable self subreaper attribute - might wait for indirect children a long time");
+	}
 
 	pid_t exit_pid = fork();
 	if (exit_pid < 0) {