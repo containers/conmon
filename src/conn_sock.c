containers/conmon

commit fad6bac8e65f3ee58845312d0ce60571052fc2ab

fix some issues flagged by SAST scan
Signed-off-by: Peter Hunt <pehunt@redhat.com>

src/conn_sock.c
@@ -263,7 +263,7 @@ static char *bind_unix_socket(char *socket_relative_name, int sock_type, mode_t 
 	int socket_fd = -1;
 
 	/* get the parent_dir of the socket. We'll use this to get the location of the socket. */
-	char *parent_dir = socket_parent_dir(use_full_attach_path, max_socket_path_len());
+	_cleanup_free_ char *parent_dir = socket_parent_dir(use_full_attach_path, max_socket_path_len());
 
 	/*
 	 * To be able to access the location of the attach socket, without first creating the attach socket

src/ctr_stdio.c
@@ -106,8 +106,8 @@ static void drain_log_buffers(stdpipe_t pipe)
 {
 	/* We pass a single byte buffer because write_to_logs expects that there is one
 	   byte of capacity beyond the buflen that we specify */
-	char buf;
-	write_to_logs(pipe, &buf, 0);
+	char buf[1];
+	write_to_logs(pipe, buf, 0);
 }
 
 static bool read_stdio(int fd, stdpipe_t pipe, gboolean *eof)

commit 8557c117bcab03e3c16e40ffb7bd450d697f72c7

Fix incorrect free in conn_sock
Earlier commit freed socket_parent_dir()'s result which is correct in
the case it returns a path from g_build_filename, but when it returns
opt_bundle_path the string should not be freed.

Make the function always return an allocated string that can be freed

Fixes: #475
Fixes: fad6bac8e65f ("fix some issues flagged by SAST scan")
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

src/conn_sock.c
@@ -314,7 +314,7 @@ char *socket_parent_dir(gboolean use_full_attach_path, size_t desired_len)
 {
 	/* if we're to use the full path, ignore the socket path and only use the bundle_path */
 	if (use_full_attach_path)
-		return opt_bundle_path;
+		return strdup(opt_bundle_path);
 
 	char *base_path = g_build_filename(opt_socket_path, opt_cuuid, NULL);
 