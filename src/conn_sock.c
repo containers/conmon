ec54d8f7a7f1cccb401c51975702a34efefaa95b
Jindrich Novy <jnovy@redhat.com>
2026-02-23 20:30:59 +0100

    Fix FreeBSD 15.0+ SOCK_SEQPACKET message boundary issue
    On FreeBSD 15.0+, SOCK_SEQPACKET sockets require MSG_EOR flag to mark
    message boundaries. Using write() without MSG_EOR causes multiple
    messages to coalesce when reader is slow, leading to frame
    desynchronization and "output destination cannot be nil" errors.
    
    This fix uses send() with MSG_EOR on FreeBSD while keeping the original
    write_all() behavior on other platforms.
    
    Fixes: containers/podman#27918
    
    Signed-off-by: Jindrich Novy <jnovy@redhat.com>

--- src/conn_sock.c
+++ src/conn_sock.c
@@ -363,7 +363,11 @@ void write_back_to_remote_consoles(char *buf, int len)
 	for (int i = local_mainfd_stdin.readers->len; i > 0; i--) {
 		struct remote_sock_s *remote_sock = g_ptr_array_index(local_mainfd_stdin.readers, i - 1);
 
+#ifdef __FreeBSD__
+		if (remote_sock->writable && send(remote_sock->fd, buf, len, MSG_EOR) < 0) {
+#else
 		if (remote_sock->writable && write_all(remote_sock->fd, buf, len) < 0) {
+#endif
 			nwarn("Failed to write to remote console socket");
 			remote_sock_shutdown(remote_sock, SHUT_WR);
 		}
